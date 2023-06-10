0ed92f5296ca1a610a0ee7e5b78d5335bbcbd6ca
Jez Ree Maghuyop <jezreelmaghuyop@outlook.com>
2023-06-10 15:07:09 +0200

    fix env prod settings and file replacement config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,4 +4,8 @@ export const environment = {
     user: '',
     weather: '',
   },
+  external: {
+    weather: 'https://api.weatherapi.com/v1/forecast.json',
+    apikey: 'f821c5fef12645d5adc61503210412',
+  },
 };
